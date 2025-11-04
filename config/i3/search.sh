438535a529008aba938b00498f1eb0dd4612d486
svxezm <igorb.kuhl@gmail.com>
2025-11-04 16:23:40 -0300

    [i3]: trigger quick search (path subject to change)

---
+++ config/i3/search.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# SEARCH_PATH=$(alias | grep search | cut -d "=" -f 2)
+# echo $SEARCH_PATH
+# alacritty -e $SEARCH_PATH
+kitty -e /media/OS/codes/languages/rust/projects/search/target/release/search
