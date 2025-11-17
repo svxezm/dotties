4f5b988a24ff76ffd1d408bdf537b30a10ef1139
svxezm <igorb.kuhl@gmail.com>
2025-11-17 15:53:01 -0300

    [i3]: edit search script

--- config/i3/search.sh
+++ config/i3/search.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-# SEARCH_PATH=$(alias | grep search | cut -d "=" -f 2)
-# echo $SEARCH_PATH
-# alacritty -e $SEARCH_PATH
-kitty -e /media/OS/codes/languages/rust/projects/search/target/release/search
+cd /media/OS/codes/languages/rust/projects/search && kitty -e ./target/release/search
