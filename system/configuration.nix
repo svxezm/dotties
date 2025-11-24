d76fb9bdf622149fee014dfe60050f4210ac3275
svxezm <igorb.kuhl@gmail.com>
2025-11-24 15:11:30 -0300

    [nix]: group options at main configuration

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -10,7 +6,6 @@
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
   boot = {
     loader = {
       systemd-boot.enable = true;
@@ -25,54 +20,51 @@
       options v4l2loopback exclusive_caps=1 card_label="OBS Virtual Camera"
     '';
   };
-  
 
+  networking = {
+    hostName = "nixos"; # Define your hostname.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/Sao_Paulo";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
+    networkmanager.enable = true;
   };
 
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
+  time.timeZone = "America/Sao_Paulo";
 
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc
-    ];
+  i18n = {
+    defaultLocale = "pt_BR.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "pt_BR.UTF-8";
+      LC_IDENTIFICATION = "pt_BR.UTF-8";
+      LC_MEASUREMENT = "pt_BR.UTF-8";
+      LC_MONETARY = "pt_BR.UTF-8";
+      LC_NAME = "pt_BR.UTF-8";
+      LC_NUMERIC = "pt_BR.UTF-8";
+      LC_PAPER = "pt_BR.UTF-8";
+      LC_TELEPHONE = "pt_BR.UTF-8";
+      LC_TIME = "pt_BR.UTF-8";
+    };
 
-    fcitx5.settings.inputMethod = {
-      GroupOrder."0" = "Default";
-      "Groups/0" = {
-        Name = "Default";
-        "Default Layout" = "us-colemak";
-        DefaultIM = "keyboard-us-colemak";
+    inputMethod = {
+      enable = true;
+      type = "fcitx5";
+
+      fcitx5.addons = with pkgs; [
+        fcitx5-mozc
+      ];
+
+      fcitx5.settings.inputMethod = {
+        GroupOrder."0" = "Default";
+        "Groups/0" = {
+          Name = "Default";
+          "Default Layout" = "us-colemak";
+          DefaultIM = "keyboard-us-colemak";
+        };
+        "Groups/0/Items/0".Name = "keyboard-us-colemak";
+        "Groups/0/Items/1".Name = "mozc";
       };
-      "Groups/0/Items/0".Name = "keyboard-us-colemak";
-      "Groups/0/Items/1".Name = "mozc";
     };
   };
 
@@ -84,56 +76,57 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Configure keymap in X11
-  services.xserver = {
-    enable = true;
-
-    videoDrivers = ["nvidia"];
-
-    desktopManager = {
-      xterm.enable = false;
-      runXdgAutostartIfNone = true;
-    };
-
-    windowManager.i3 = {
+  services = {
+    xserver = {
       enable = true;
-      extraPackages = with pkgs; [
-        dmenu
-        i3status
-        i3blocks
-      ];
-    };
 
-    xkb = {
-      layout = "us";
-      variant = "colemak";
-      model = "pc104";
-    };
-  };
+      videoDrivers = ["nvidia"];
 
-  services.displayManager.defaultSession = "none+i3";
+      desktopManager = {
+        xterm.enable = false;
+        runXdgAutostartIfNone = true;
+      };
 
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          dmenu
+          i3status
+          i3blocks
+        ];
+      };
 
-  services.openssh.enable = true;
-  services.httpd = {
-    enable = true;
-    virtualHosts = {
-      "localhost" = {
-        documentRoot = "/srv/http";
-        extraConfig = ''
-          <FilesMatch "\.php$">
-            SetHandler application/x-httpd-php
-          </FilesMatch>
-        '';
+      xkb = {
+        layout = "us";
+        variant = "colemak";
+        model = "pc104";
       };
     };
-    extraModules = [ "php" ];
+
+    displayManager.defaultSession = "none+i3";
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+
+    openssh.enable = true;
+    httpd = {
+      enable = true;
+      virtualHosts = {
+        "localhost" = {
+          documentRoot = "/srv/http";
+          extraConfig = ''
+            <FilesMatch "\.php$">
+              SetHandler application/x-httpd-php
+            </FilesMatch>
+          '';
+        };
+      };
+      extraModules = [ "php" ];
+    };
   };
 
   programs = {
@@ -145,9 +138,14 @@
 
     nix-ld.enable = true;
     nix-ld.libraries = with pkgs; [];
+
+    mtr.enable = true;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
   };
 
-  # Configure console keymap
   console = {
     useXkbConfig = true;
   };
@@ -164,35 +162,29 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    wget
-    git
-    playerctl
-    xclip
-    fcitx5-mozc
+  environment = {
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    systemPackages = with pkgs; [
+      wget
+      git
+      playerctl
+      xclip
+      fcitx5-mozc
 
-    cifs-utils
-    ntfs3g
-  ];
+      # cifs-utils
+      # ntfs3g
+    ];
 
-  environment.etc.fstab.text =
-    "/dev/nvme0n1p4 /media/OS ntfs-3g defaults,uid=1000,gid=1000,nofail 0 0";
+    # etc.fstab.text =
+    #   "/dev/nvme0n1p4 /media/OS ntfs-3g defaults,uid=1000,gid=1000,nofail 0 0";
 
-  environment.sessionVariables = {
-    GTK_IM_MODULE = "fcitx";
-    QT_IM_MODULE = "fcitx";
-    XMODIFIERS = "@im=fcitx";
-    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-  };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
+    sessionVariables = {
+      GTK_IM_MODULE = "fcitx";
+      QT_IM_MODULE = "fcitx";
+      XMODIFIERS = "@im=fcitx";
+      PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+    };
   };
 
   hardware = {
