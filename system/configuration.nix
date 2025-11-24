80672b0e7edddfbfe1483a2ad15341f93519a8dc
svxezm <igorb.kuhl@gmail.com>
2025-11-24 14:30:10 -0300

    [nix]: setup v4l2loopback for obs

--- system/configuration.nix
+++ system/configuration.nix
@@ -11,8 +11,22 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    extraModulePackages = with config.boot.kernelPackages; [
+      v4l2loopback
+    ];
+    kernelModules = [ "v4l2loopback" ];
+    extraModprobeConfig = ''
+      options v4l2loopback exclusive_caps=1 card_label="OBS Virtual Camera"
+    '';
+  };
+  
+
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
